daa87110bd767af07584b6316d249514ff8ff147
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-05-25 00:37:41 +0800

    added name hover effects

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function easeIn() {
     document.body.classList.add('loaded');
 }
+
+const nameElement = document.getElementById('interactive-name');
+  const letters = nameElement.textContent.split('');
+  nameElement.innerHTML = letters
+    .map(char => {
+      if (char === ' ') return '&nbsp;';
+      return `<span class="letter">${char}</span>`;
+    })
+    .join('');
